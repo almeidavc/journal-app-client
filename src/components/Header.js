01372f636e638404d1b8779ff2806ca019cb46de
Vicente Almeida <vicentecostaalmeida@gmail.com>
2022-05-28 12:48:27 +0200

    add route to '/' by clicking the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <h1 className="mt-8 mb-3 text-3xl">Journal App</h1>;
+  return (
+    <Link to="/">
+      <h1 className="mt-8 mb-3 text-3xl">Journal App</h1>
+    </Link>
+  );
 };
 
 export default Header;
